95d59ce41bf6c2ddda67b3d85bc32b2bf2fdda1b
swarup <swarup260@gmail.com>
2021-10-19 23:39:26 +0530

    initial animation

--- lib/widgets/animation.dart
+++ lib/widgets/animation.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:notes/utils/notes_color.dart';
+/* 
+https://www.npmjs.com/package/commander
+https://www.npmjs.com/package/treeify
+https://www.npmjs.com/package/chalk
+https://www.npmjs.com/package/inquirer
+https://github.com/jondot/hygen
+
+ */
 
 // ignore: must_be_immutable
 class AnimationPage extends StatefulWidget {
@@ -14,7 +22,6 @@ class _AnimationPageState extends State<AnimationPage>
     with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation<double> animation;
-  Animation<double> animationRadius;
 
   @override
   void initState() {
@@ -26,22 +33,6 @@ class _AnimationPageState extends State<AnimationPage>
       ..addListener(() {
         setState(() {});
       });
-    // ..addStatusListener((status) {
-    //   print('$status');
-    //   if (status == AnimationStatus.completed) {
-    //     controller.reverse();
-    //   }
-    //   if (status == AnimationStatus.dismissed) {
-    //     controller.forward();
-    //   }
-    // });
-
-    /* Animation curveAnimation =
-        CurvedAnimation(parent: controller, curve: Curves.bounceIn); */
-
-    animationRadius = Tween<double>(begin: 100, end: 0).animate(controller);
-
-    // controller.forward();
   }
 
   @override
@@ -50,26 +41,37 @@ class _AnimationPageState extends State<AnimationPage>
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Align(
-        alignment: Alignment.bottomRight,
-        child: Container(
-            width: animation.value,
-            height: animation.value,
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: IconButton(
-                icon: Icon(Icons.cancel),
-                onPressed: () => {controller.reverse()},
-              ),
-            ),
-            // color: NotesColor.noteColor,
-            decoration: BoxDecoration(
-              color: NotesColor.noteColor[700],
-              borderRadius: BorderRadius.circular(animationRadius.value),
+      body: Center(
+        child: Hero(
+            tag: 'add-new-todo',
+            child: Container(
+              width: 300,
+              height: 300,
+              color: NotesColor.noteColor[100],
             )),
       ),
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add), onPressed: () => {controller.forward()}),
+          child: Icon(Icons.add), onPressed: () => {_gotoAddToDo(context)}),
     );
+  }
+
+  void _gotoAddToDo(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+        builder: (BuildContext context) => Scaffold(
+              appBar: AppBar(
+                title: Text('Add ToDo'),
+              ),
+              body: Center(
+                child: Hero(
+                    tag: 'add-new-todo',
+                    child: Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          color: NotesColor.noteColor[100],
+                          borderRadius: BorderRadius.circular(100)),
+                    )),
+              ),
+            )));
   }
 }
